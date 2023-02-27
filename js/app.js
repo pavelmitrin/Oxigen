5e43de52a33682626b446653e24f34c2805397ab
pavelmitrin <mitrinp@outlook.com>
2023-02-27 23:40:01 +0300

    done all without carousels and modals

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+
+// toggle header
+function checkScroll() {
+	if (window.scrollY > 500) {
+		header.classList.remove("d-none");
+	} else {
+		header.classList.add("d-none");
+	}
+}
+const header = document.querySelector(".header");
+window.addEventListener('scroll', checkScroll);
+window.addEventListener('load', checkScroll);
