525ad37dc4911a9c858b787f071c70bc42c15f92
pavelmitrin <mitrinp@outlook.com>
2023-03-03 03:43:51 +0300

    all done,  finally)))

--- js/app.js
+++ js/app.js
@@ -1,6 +1,61 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
+/* modal */
+
+const modalPresentation = document.querySelector("#presentation");
+const imgWrapper = modalPresentation.querySelector(".order-lg-1");
+
+function modalImgAbsolute() {
+	if (window.innerWidth < 992) {
+		imgWrapper.classList.add("position-absolute", "w-75", "top-0", "start-50", "translate-middle");
+	} else {
+		imgWrapper.classList.remove("position-absolute", "w-75", "top-0", "start-50", "translate-middle");
+	}
+}
+// window.addEventListener('load', modalImgAbsolute);
+// window.addEventListener('resize', modalImgAbsolute)
+/* .modal */
+
+const swiperOne = new Swiper('.o-swiper-carouselOne', {
+	effect: "coverflow",  
+	loop: true,
+	centeredSlides: true,
+	slidesPerView: 1,
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
+	},
+	autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+   },
+	spaceBetween: 10,
+	breakpoints: {
+		768: {
+			slidesPerView: 1.25
+		}
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+const swiper = new Swiper('.o-progress__swiper', {
+	loop: true,
+	centeredSlides: true,
+	slidesPerView: 1,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+	}
+});
 
 // toggle header
 function checkScroll() {
@@ -13,3 +68,122 @@ function checkScroll() {
 const header = document.querySelector(".header");
 window.addEventListener('scroll', checkScroll);
 window.addEventListener('load', checkScroll);
+
+
+/* modal */
+const exampleModal = document.getElementById('presentation')
+exampleModal.addEventListener('show.bs.modal', event => {
+	// Кнопка, которая активировала модальное окно
+	const button = event.relatedTarget
+	// Извлекает информацию из атрибутов data-bs-*
+	const recipient = button.getAttribute('data-bs-whatever')
+	const recipient0 = recipient.replace('@', '');
+	/* data */
+	const allWhateverValue = {
+		call: {
+			name: "Обратный звонок",
+			text: "Как правило перезваниваем в течение 15 минут",
+			img: "img/modals/phone.png",
+			btnText: "Отправить",
+		},
+		presentation: {
+			name: "Скачайте презентацию",
+			text: "Укажите свои контактные данные и скачайте презентацию <strong>в один клик</strong>",
+			img: "img/modals/1.png",
+			btnText: "Скачать презентацию",
+		},
+		layout: {
+			name: "Планировки и цены",
+			text: "Наш менеджер свяжется, уточнит желаемые планировки и отправит информацию",
+			img: "img/modals/plans.jpg",
+			btnText: "Получить",
+		},
+		show: {
+			name: "Запись на показ",
+			text: "Наш менеджер свяжется и Вы запланируете удобную дату и время показа",
+			img: "img/modals/phone.png",
+			btnText: "Отправить",
+		},
+	}
+	let name =  allWhateverValue[recipient0].name;
+	let text = allWhateverValue[recipient0].text;
+	let img = allWhateverValue[recipient0].img;
+	let btnText = allWhateverValue[recipient0].btnText;
+
+	//
+	// Обновляет содержимое модального окна.
+	const modalTitle = exampleModal.querySelector('.c-modal__title')
+	const modalText = exampleModal.querySelector('.c-presentation__text');
+	const modalImg = exampleModal.querySelector('.c-presentation-modal__img');
+	const modalBtn = exampleModal.querySelector('.c-presentation__modal-btn');
+
+	for (const key in allWhateverValue) {
+		exampleModal.classList.remove(`c-presentation-modal__img-${key}`);
+	}
+	exampleModal.classList.add(`c-presentation-modal__img-${recipient0}`);
+
+
+	modalTitle.textContent = name;
+	modalText.innerHTML = `${text}`;
+	modalImg.setAttribute('src', `${img}`);
+	modalBtn.textContent = btnText;
+
+})
+
+/* modalCarousel */
+const exampleModalCarousel = document.getElementById('prevLayoutsCarousel');
+exampleModalCarousel.addEventListener('show.bs.modal', event => {
+	// Кнопка, которая активировала модальное окно
+	const button = event.relatedTarget
+	// Извлекает информацию из атрибутов data-bs-*
+	const recipient = button.getAttribute('data-o-layout-num');
+	/* data */
+	const countImage = {
+		One: 4,
+		Two: 4,
+		Three: 4,
+		Four: 4,
+	}
+	let count = countImage[recipient];
+	const modalSlideWrapper = exampleModalCarousel.querySelector(".swiper-wrapper");
+	modalSlideWrapper.innerHTML = "";
+	// Обновляем содержимое модального окна.
+	for (let index = 1; index < count; index++) {
+		modalSlideWrapper.insertAdjacentHTML('beforeend', `<div class="swiper-slide">
+		<img src="img/layouts/${recipient}/${index}.jpg" class="img-fluid" alt="">
+	</div>`
+		)
+	}
+
+
+	const ModalSwiper = new Swiper('.o-modal-carousel__container', {
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	slidesPerView: 1,
+	centeredSlides: true,
+});
+})
+
+/* inputMask */
+const telInput = document.querySelector("#modalTel");
+const telMask = new Inputmask("+7(999)999-99-99");
+telMask.mask(telInput);
+
+
+/* validate form */
+const forms = document.querySelectorAll('.needs-validation')
+
+// Loop over them and prevent submission
+Array.from(forms).forEach(form => {
+   form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+			event.preventDefault()
+			event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+	}, false)
+})
